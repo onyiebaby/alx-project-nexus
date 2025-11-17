ae2c6d9b77417709bdff588944b029c83ddb7e36
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-11-17 17:24:16 +0100

    Add all project files

--- fashionworld/settings.py
+++ fashionworld/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u&a0es#cm2!rmttoo(&%b$6kx()g3zr^u-y2tb5_8x)mp55w#p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'fashionworld-5vz8.onrender.com',
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
